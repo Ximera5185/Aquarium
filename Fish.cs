423391a4f5e90735893d51016219869878020177
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-04-20 16:31:51 +0300

    1

---
+++ Fish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium
+{
+    internal class Fish
+    {
+        public Fish(string name, int health) 
+        {
+            Name = name;
+
+            Heath = health;
+        }
+
+        public string Name { get; private set;}
+        public int Heath { get; private set;}
+
+    }
+}
---
+++ UserUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aquarium
+{
+    static class UserUtils
+    {
+        private static Random s_random = new Random();
+
+        static public int GetRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue,maxValue);
+        }
+    }
+}
